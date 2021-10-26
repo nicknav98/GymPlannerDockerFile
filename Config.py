d2de4cc92b0369c3103fec7863810dce933f20a4
Nicholas Navarro <nicholasnavarro98@gmail.com>
2021-10-26 12:05:28 +0300

    added .yaml file and necessary adjustments in Config.py to able to function inside our docker network

--- Config.py
+++ Config.py
@@ -1,7 +1,7 @@
 class Config:
     DEBUG = True
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nicknav98:5432@localhost/WorkoutsDB'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://admin:password@localhost/WorkoutsDB'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SECRET_KEY = 'super-secret-key'
